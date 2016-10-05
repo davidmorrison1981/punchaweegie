98ec623861b31eb5784b0bea323f31d759c05c3e
Alan Russell <ajrussellaudio@gmail.com>
2016-10-05 11:33:45 +0100

    add array of cats

--- cat-api/catApi.js
+++ cat-api/catApi.js
@@ -1,5 +1,20 @@
 var CatApi = function( app ) {
 
+  var cats = [
+  {
+    name: "James",
+    breed: "Siamese"
+  },
+  {
+    name: "Nizmo",
+    breed: "Bengal"
+  },
+  {
+    name: "Bagpuss",
+    breed: "stuffed"
+  }
+  ]
+
 }
 
 module.exports = CatApi;
